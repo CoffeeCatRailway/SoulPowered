67c9c21519369bd14cad2e9faae87615449f21bf
CoffeeCatRailway <dragonlee1002@gmail.com>
2020-12-11 13:06:41 +1100

    This shouldn't be here

--- src/main/java/coffeecatrailway/soulpowered/intergration/tinkers/package-info.java
+++
@@ -1,7 +0,0 @@
-@ParametersAreNonnullByDefault
-@MethodsReturnNonnullByDefault
-package coffeecatrailway.soulpowered.intergration.tinkers;
-
-import mcp.MethodsReturnNonnullByDefault;
-
-import javax.annotation.ParametersAreNonnullByDefault;
