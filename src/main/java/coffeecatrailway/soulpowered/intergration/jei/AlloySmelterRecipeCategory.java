0f3a6178be6524d211771ab27fcbfec4d7889f3d
CoffeeCatRailway <dragonlee1002@gmail.com>
2020-12-20 21:19:02 +1100

    Added JEI support

---
+++ src/main/java/coffeecatrailway/soulpowered/intergration/jei/AlloySmelterRecipeCategory.java
@@ -0,0 +1,111 @@
+package coffeecatrailway.soulpowered.intergration.jei;
+
+import coffeecatrailway.soulpowered.client.gui.screen.AlloySmelterScreen;
+import coffeecatrailway.soulpowered.common.item.crafting.AlloySmelterRecipe;
+import coffeecatrailway.soulpowered.registry.SoulBlocks;
+import com.mojang.blaze3d.matrix.MatrixStack;
+import mezz.jei.api.constants.VanillaTypes;
+import mezz.jei.api.gui.IRecipeLayout;
+import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.gui.drawable.IDrawableAnimated;
+import mezz.jei.api.gui.ingredient.IGuiItemStackGroup;
+import mezz.jei.api.helpers.IGuiHelper;
+import mezz.jei.api.ingredients.IIngredients;
+import mezz.jei.api.recipe.category.IRecipeCategory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.ResourceLocation;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * @author CoffeeCatRailway
+ * Created: 18/12/2020
+ */
+public class AlloySmelterRecipeCategory implements IRecipeCategory<AlloySmelterRecipe>
+{
+    private final IDrawable background;
+    private final IDrawable icon;
+
+    private final IDrawableAnimated progress;
+
+    private final String localizedName;
+
+    public AlloySmelterRecipeCategory(IGuiHelper guiHelper)
+    {
+        this.background = guiHelper.createDrawable(AlloySmelterScreen.TEXTURE, 57, 13, 62, 56);
+        this.icon = guiHelper.createDrawableIngredient(new ItemStack(SoulBlocks.ALLOY_SMELTER.get()));
+
+        this.progress = guiHelper.drawableBuilder(AlloySmelterScreen.TEXTURE, 176, 0, 46, 20)
+                .buildAnimated(200, IDrawableAnimated.StartDirection.TOP, true);
+
+        this.localizedName = SoulBlocks.ALLOY_SMELTER.get().getTranslatedName().getString();
+    }
+
+    @Override
+    public ResourceLocation getUid()
+    {
+        return JeiSoulPlugin.ALLOY_SMELTER_ID;
+    }
+
+    @Override
+    public Class<? extends AlloySmelterRecipe> getRecipeClass()
+    {
+        return AlloySmelterRecipe.class;
+    }
+
+    @Override
+    public String getTitle()
+    {
+        return this.localizedName;
+    }
+
+    @Override
+    public IDrawable getBackground()
+    {
+        return this.background;
+    }
+
+    @Override
+    public IDrawable getIcon()
+    {
+        return this.icon;
+    }
+
+    @Override
+    public void setIngredients(AlloySmelterRecipe recipe, IIngredients ingredients)
+    {
+        ingredients.setInputLists(VanillaTypes.ITEM, recipe.getIngredientsMap().keySet().stream().map(ingredient -> Arrays.asList(ingredient.getMatchingStacks())).collect(Collectors.toList()));
+        ingredients.setOutput(VanillaTypes.ITEM, recipe.getRecipeOutput());
+    }
+
+    @Override
+    public void setRecipe(IRecipeLayout layout, AlloySmelterRecipe recipe, IIngredients ingredients)
+    {
+        IGuiItemStackGroup group = layout.getItemStacks();
+        group.init(0, true, 1, 1);
+        group.init(1, true, 22, 1);
+        group.init(2, true, 43, 1);
+        group.init(3, false, 22, 37);
+
+        int i = 0;
+        for (Map.Entry<Ingredient, Integer> entry : recipe.getIngredientsMap().entrySet())
+        {
+            group.set(i++, Arrays.stream(entry.getKey().getMatchingStacks()).map(s -> {
+                        ItemStack stack = s.copy();
+                        stack.setCount(entry.getValue());
+                        return stack;
+                    }).collect(Collectors.toList())
+            );
+        }
+        group.set(3, recipe.getRecipeOutput());
+    }
+
+    @Override
+    public void draw(AlloySmelterRecipe recipe, MatrixStack matrixStack, double mouseX, double mouseY)
+    {
+        this.progress.draw(matrixStack, 8, 18);
+    }
+}
---
+++ src/main/java/coffeecatrailway/soulpowered/intergration/jei/JeiSoulPlugin.java
@@ -0,0 +1,75 @@
+package coffeecatrailway.soulpowered.intergration.jei;
+
+import coffeecatrailway.soulpowered.SoulPoweredMod;
+import coffeecatrailway.soulpowered.client.gui.screen.AlloySmelterScreen;
+import coffeecatrailway.soulpowered.common.inventory.container.AlloySmelterContainer;
+import coffeecatrailway.soulpowered.registry.SoulBlocks;
+import coffeecatrailway.soulpowered.registry.SoulRecipes;
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.helpers.IGuiHelper;
+import mezz.jei.api.registration.*;
+import net.minecraft.client.Minecraft;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.item.crafting.IRecipeType;
+import net.minecraft.util.ResourceLocation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author CoffeeCatRailway
+ * Created: 18/12/2020
+ */
+@JeiPlugin
+public class JeiSoulPlugin implements IModPlugin
+{
+    private static final ResourceLocation UID = SoulPoweredMod.getLocation("plugin/main");
+
+    static final ResourceLocation ALLOY_SMELTER_ID = SoulPoweredMod.getLocation("category/alloy_smelter");
+
+    @Override
+    public ResourceLocation getPluginUid()
+    {
+        return UID;
+    }
+
+    @Override
+    public void registerCategories(IRecipeCategoryRegistration registration)
+    {
+        IGuiHelper guiHelper = registration.getJeiHelpers().getGuiHelper();
+        registration.addRecipeCategories(new AlloySmelterRecipeCategory(guiHelper));
+    }
+
+    @Override
+    public void registerRecipes(IRecipeRegistration registration)
+    {
+        registration.addRecipes(getRecipesOfType(SoulRecipes.ALLOY_SMELTING_TYPE), ALLOY_SMELTER_ID);
+    }
+
+    private static List<IRecipe<?>> getRecipesOfType(IRecipeType<?> type)
+    {
+        if (Minecraft.getInstance().world == null) return new ArrayList<>();
+        return Minecraft.getInstance().world.getRecipeManager().getRecipes().stream().filter(recipe -> recipe.getType() == type).collect(Collectors.toList());
+    }
+
+    @Override
+    public void registerRecipeTransferHandlers(IRecipeTransferRegistration registration)
+    {
+        registration.addRecipeTransferHandler(AlloySmelterContainer.class, ALLOY_SMELTER_ID, 0, 3, 4, 35);
+    }
+
+    @Override
+    public void registerRecipeCatalysts(IRecipeCatalystRegistration registration)
+    {
+        registration.addRecipeCatalyst(new ItemStack(SoulBlocks.ALLOY_SMELTER.get()), ALLOY_SMELTER_ID);
+    }
+
+    @Override
+    public void registerGuiHandlers(IGuiHandlerRegistration registration)
+    {
+        registration.addRecipeClickArea(AlloySmelterScreen.class, 65, 31, 46, 20, ALLOY_SMELTER_ID);
+    }
+}
