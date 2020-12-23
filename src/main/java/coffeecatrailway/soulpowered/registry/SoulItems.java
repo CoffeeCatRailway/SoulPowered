52e8b2cbb753870ef6fca17ad2a07e2541a9240c
CoffeeCatRailway <dragonlee1002@gmail.com>
2020-12-23 20:22:38 +1100

    Re-worked netherite & powered soul amulet recipes

--- src/main/java/coffeecatrailway/soulpowered/registry/SoulItems.java
+++ src/main/java/coffeecatrailway/soulpowered/registry/SoulItems.java
@@ -2,6 +2,7 @@ package coffeecatrailway.soulpowered.registry;
 
 import coffeecatrailway.soulpowered.SoulData;
 import coffeecatrailway.soulpowered.SoulPoweredMod;
+import coffeecatrailway.soulpowered.api.Tier;
 import coffeecatrailway.soulpowered.common.item.*;
 import com.tterrag.registrate.Registrate;
 import com.tterrag.registrate.builders.ItemBuilder;
@@ -13,10 +14,11 @@ import com.tterrag.registrate.util.entry.RegistryEntry;
 import com.tterrag.registrate.util.nullness.NonNullBiConsumer;
 import com.tterrag.registrate.util.nullness.NonNullFunction;
 import net.minecraft.data.ShapedRecipeBuilder;
+import net.minecraft.data.SmithingRecipeBuilder;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemTier;
 import net.minecraft.item.Items;
-import net.minecraft.item.Rarity;
+import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.util.IItemProvider;
 import net.minecraftforge.common.Tags;
@@ -69,26 +71,32 @@ public class SoulItems
             .tag(SoulData.TagItems.SOUL_GENERATOR_FUEL).register();
 
     public static final RegistryEntry<SoulAmuletItem> GOLD_SOUL_AMULET = registerSoulAmulet("gold_soul_amulet", prop -> new SoulAmuletItem(prop, ItemTier.GOLD, 1.5f, .1f,
-                    SoulPoweredMod.getLocation("textures/models/gold_soul_amulet.png")), "Golden Soul Amulet", () -> Items.GOLD_INGOT)
+            SoulPoweredMod.getLocation("textures/models/gold_soul_amulet.png")), "Golden Soul Amulet", () -> Items.GOLD_INGOT)
             .tag(ItemTags.PIGLIN_LOVED).register();
 
     public static final RegistryEntry<SoulAmuletItem> COPPER_SOUL_AMULET = registerSoulAmulet("copper_soul_amulet", prop -> new SoulAmuletItem(prop, 100, 1.5f, .15f,
-                    SoulPoweredMod.getLocation("textures/models/copper_soul_amulet.png")), "Copper Soul Amulet", COPPER_INGOT::get).register();
+            SoulPoweredMod.getLocation("textures/models/copper_soul_amulet.png")), "Copper Soul Amulet", COPPER_INGOT::get).register();
 
     public static final RegistryEntry<SoulAmuletItem> IRON_SOUL_AMULET = registerSoulAmulet("iron_soul_amulet", prop -> new SoulAmuletItem(prop, ItemTier.IRON, 1.75f, .25f,
-                    SoulPoweredMod.getLocation("textures/models/iron_soul_amulet.png")), "Iron Soul Amulet", () -> Items.IRON_INGOT).register();
+            SoulPoweredMod.getLocation("textures/models/iron_soul_amulet.png")), "Iron Soul Amulet", () -> Items.IRON_INGOT).register();
 
     public static final RegistryEntry<SoulAmuletItem> DIAMOND_SOUL_AMULET = registerSoulAmulet("diamond_soul_amulet", prop -> new SoulAmuletItem(prop, ItemTier.DIAMOND, 2f, .5f,
-                    SoulPoweredMod.getLocation("textures/models/diamond_soul_amulet.png")), "Diamond Soul Amulet", () -> Items.DIAMOND).register();
+            SoulPoweredMod.getLocation("textures/models/diamond_soul_amulet.png")), "Diamond Soul Amulet", () -> Items.DIAMOND).register();
 
     public static final RegistryEntry<SoulAmuletItem> SOULIUM_SOUL_AMULET = registerSoulAmulet("soulium_soul_amulet", prop -> new SoulAmuletItem(prop, 1750, 2.5f, .75f,
-                    SoulPoweredMod.getLocation("textures/models/soulium_soul_amulet.png")), "Soulium Soul Amulet", SOULIUM_INGOT::get).register();
+            SoulPoweredMod.getLocation("textures/models/soulium_soul_amulet.png")), "Soulium Soul Amulet", SOULIUM_INGOT::get).register();
 
     public static final RegistryEntry<SoulAmuletPoweredItem> POWERED_SOULIUM_SOUL_AMULET = registerSoulAmulet("powered_soulium_soul_amulet", prop -> new SoulAmuletPoweredItem(prop, 3f, 1f),
-            "Powered Soulium Soul Amulet", SOULIUM_INGOT::get, BATTERY::get, NonNullBiConsumer.noop()).register();
+            "Powered Soulium Soul Amulet", SOULIUM_INGOT::get, BATTERY::get, NonNullBiConsumer.noop(), true)
+            .recipe((ctx, provider) -> SmithingRecipeBuilder.smithingRecipe(Ingredient.fromItems(SOULIUM_SOUL_AMULET.get()), Ingredient.fromItems(BATTERY.get()), ctx.getEntry())
+                    .addCriterion("has_soulium_soul_amulet", RegistrateRecipeProvider.hasItem(SOULIUM_SOUL_AMULET.get()))
+                    .addCriterion("has_battery", RegistrateRecipeProvider.hasItem(BATTERY.get())).build(provider, ctx.getId())).register();
 
     public static final RegistryEntry<SoulAmuletItem> NETHERITE_SOUL_AMULET = registerSoulAmulet("netherite_soul_amulet", prop -> new SoulAmuletItem(prop, ItemTier.NETHERITE, 3.5f, 1f,
-            SoulPoweredMod.getLocation("textures/models/netherite_soul_amulet.png")), "Netherite Soul Amulet", () -> Items.NETHERITE_INGOT).register();
+            SoulPoweredMod.getLocation("textures/models/netherite_soul_amulet.png")), "Netherite Soul Amulet", () -> Items.NETHERITE_INGOT, true)
+            .recipe((ctx, provider) -> SmithingRecipeBuilder.smithingRecipe(Ingredient.fromItems(DIAMOND_SOUL_AMULET.get()), Ingredient.fromTag(Tags.Items.INGOTS_NETHERITE), ctx.getEntry())
+                    .addCriterion("has_diamond_soul_amulet", RegistrateRecipeProvider.hasItem(DIAMOND_SOUL_AMULET.get()))
+                    .addCriterion("has_netherite", RegistrateRecipeProvider.hasItem(Tags.Items.INGOTS_NETHERITE)).build(provider, ctx.getId())).register();
 
     public static final RegistryEntry<SoulShieldItem> SOUL_SHIELD = registerCurio(REGISTRATE.item("soul_shield", prop -> new SoulShieldItem(prop, 4f, 10f, 25f))
             .tag(SoulData.TagItems.CURIOS_CHARM).defaultLang().model(NonNullBiConsumer.noop()).recipe((ctx, provider) -> ShapedRecipeBuilder.shapedRecipe(ctx.getEntry())
@@ -99,35 +107,45 @@ public class SoulItems
 
     private static <T extends Item> ItemBuilder<T, Registrate> registerCurio(ItemBuilder<T, Registrate> entry, String description)
     {
-        SoulData.Lang.EXTRA_LANGS.put("item." + SoulPoweredMod.MOD_ID + "." + entry.getName() +  ".description", description);//.getId().getPath()
+        SoulData.Lang.EXTRA_LANGS.put("item." + SoulPoweredMod.MOD_ID + "." + entry.getName() + ".description", description);
         return entry;
     }
 
     private static <T extends Item> ItemBuilder<T, Registrate> registerSoulAmulet(String id, NonNullFunction<Item.Properties, T> item, String name, Supplier<IItemProvider> ingot)
     {
         return registerSoulAmulet(id, item, name, ingot, () -> null, (ctx, provider) -> provider.generated(ctx::getEntry, SoulPoweredMod.getLocation("item/soul_amulet_gem"))
-                .texture("layer1", SoulPoweredMod.getLocation("item/" + ctx.getName())));
+                .texture("layer1", SoulPoweredMod.getLocation("item/" + ctx.getName())), false);
     }
 
-    private static <T extends Item> ItemBuilder<T, Registrate> registerSoulAmulet(String id, NonNullFunction<Item.Properties, T> item, String name, Supplier<IItemProvider> ingot, Supplier<IItemProvider> top, NonNullBiConsumer<DataGenContext<Item, T>, RegistrateItemModelProvider> model)
+    private static <T extends Item> ItemBuilder<T, Registrate> registerSoulAmulet(String id, NonNullFunction<Item.Properties, T> item, String name, Supplier<IItemProvider> ingot, boolean customRecipe)
     {
-        return registerCurio(REGISTRATE.item(id, item).tag(SoulData.TagItems.CURIOS_NECKLACE).lang(name).model(model)
-                .recipe((ctx, provider) -> {
-                    boolean flag = top.get() != null;
-                    ShapedRecipeBuilder builder = ShapedRecipeBuilder.shapedRecipe(ctx.getEntry()).key('i', ingot.get()).key('s', SOUL_BOTTLE.get());
+        return registerSoulAmulet(id, item, name, ingot, () -> null, (ctx, provider) -> provider.generated(ctx::getEntry, SoulPoweredMod.getLocation("item/soul_amulet_gem"))
+                .texture("layer1", SoulPoweredMod.getLocation("item/" + ctx.getName())), customRecipe);
+    }
 
-                    if (flag)
-                        builder.key('t', top.get()).patternLine(" t ");
-                    else
-                        builder.patternLine(" i ");
+    private static <T extends Item> ItemBuilder<T, Registrate> registerSoulAmulet(String id, NonNullFunction<Item.Properties, T> item, String name, Supplier<IItemProvider> ingot, Supplier<IItemProvider> top, NonNullBiConsumer<DataGenContext<Item, T>, RegistrateItemModelProvider> model, boolean customRecipe)
+    {
+        ItemBuilder<T, Registrate> builder = REGISTRATE.item(id, item).tag(SoulData.TagItems.CURIOS_NECKLACE).lang(name).model(model);
 
-                    builder.patternLine("i i").patternLine(" s ")
-                            .addCriterion("has_soul_bottle", RegistrateRecipeProvider.hasItem(SOUL_BOTTLE.get()))
-                            .addCriterion("has_ingot", RegistrateRecipeProvider.hasItem(ingot.get()));
-                    if (flag)
-                        builder.addCriterion("has_top", RegistrateRecipeProvider.hasItem(top.get()));
-                    builder.build(provider);
-                }), "Allows you to gather souls from your kills");
+        if (!customRecipe)
+            builder = builder.recipe((ctx, provider) -> {
+                boolean flag = top.get() != null;
+                ShapedRecipeBuilder recipeBuilder = ShapedRecipeBuilder.shapedRecipe(ctx.getEntry()).key('i', ingot.get()).key('s', SOUL_BOTTLE.get());
+
+                if (flag)
+                    recipeBuilder.key('t', top.get()).patternLine(" t ");
+                else
+                    recipeBuilder.patternLine(" i ");
+
+                recipeBuilder.patternLine("i i").patternLine(" s ")
+                        .addCriterion("has_soul_bottle", RegistrateRecipeProvider.hasItem(SOUL_BOTTLE.get()))
+                        .addCriterion("has_ingot", RegistrateRecipeProvider.hasItem(ingot.get()));
+                if (flag)
+                    recipeBuilder.addCriterion("has_top", RegistrateRecipeProvider.hasItem(top.get()));
+                recipeBuilder.build(provider);
+            });
+
+        return registerCurio(builder, "Allows you to gather souls from your kills");
     }
 
     public static void load()
